562ac42d8f8f4dc867b4f2bac5099b1e0a180872
hayward5 <hayward5.cs10@nycu.edu.tw>
2026-04-04 20:04:40 +0800

    chore: add week7 schema fixture and parser export

--- src/dataguard/parser/__init__.py
+++ src/dataguard/parser/__init__.py
@@ -1,9 +1,11 @@
 from dataguard.parser.base import BaseParser, ParseErrorItem, ParseResult
+from dataguard.parser.encoding import detect_encoding
 from dataguard.parser.factory import get_parser
 
 __all__ = [
     "BaseParser",
     "ParseErrorItem",
     "ParseResult",
+    "detect_encoding",
     "get_parser",
 ]
